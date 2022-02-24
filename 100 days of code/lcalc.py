2cfc2d50fcd58dfb16b34b8e268a880ed6324c66
TARUN KOTAGIRI <tarunkotagiri007@gmail.com>
2022-02-24 09:19:10 +0530

    added files

--- 100 days of code/lcalc.py
+++ 100 days of code/lcalc.py
@@ -6,12 +6,24 @@ name2 = input("What is their name? \n")
 
 #Write your code below this line 👇
 
-name1 = name1.lower()
-name2 = name2.lower()
+combined_name = name1 + name2
+combined_name = combined_name.lower()
 
-i = 0
+i,j = 0,0
 
 for cnt in "true":
-    i += name1.count(cnt)
+    i += combined_name.count(cnt)
+    
 
-print(f"Digit 1:  {i}.")
+for cnt in "love":
+    j += combined_name.count(cnt)
+
+
+score = int(f"{i}{j}")
+static_statement = f"Your score is {score}"
+if(score < 10 or score > 90):
+    print(static_statement + ", you go together like coke and mentos.")
+elif(score > 40 and score < 50):
+    print(static_statement + ", you are alright together.")
+else:
+    print(static_statement + ".")
